887252836dcfab444fb73f2222501020d5714967
vannguyenv12 <npnv.vn1@gmail.com>
2024-04-23 17:51:56 +0700

    feat: Statistics Dashboard

---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import { signup as signupApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+function useSignup() {
+  const { mutate: signup, isLoading } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: (user) => {
+      console.log(user);
+      toast.success(
+        "Account successfully created! Please verify your email address"
+      );
+    },
+  });
+
+  return { signup, isLoading };
+}
+
+export default useSignup;
